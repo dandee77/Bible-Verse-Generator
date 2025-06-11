c94c881b0cfd6aa242f09cb54feda803023256d8
Dandee Galang <dandeegalang.ph@gmail.com>
2025-06-11 21:12:27 +0800

    added data fetching from unsplash api and a public api key

---
+++ Infinity_Scroll/script.js
@@ -0,0 +1,18 @@
+
+// Unsplash API
+const count = 10;
+const apiKey = 'Y253f7jsAioJhn_KaLqrnlhRrs_qPQ9OsEPnC1Ul2Uc';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//  get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching photos:', error);
+    }
+}
+
+getPhotos();
