6753af103d034795c0cfa63f771a27b67d77fd4c
Dandee Galang <dandeegalang.ph@gmail.com>
2025-06-12 02:22:30 +0800

    added js functionality

---
+++ Picture_In_Picture/script.js
@@ -0,0 +1,33 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// prompt the user to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = async () => {
+        videoElement.play();
+        button.disabled = true;
+        await videoElement.requestPictureInPicture();
+        button.disabled = false;
+    };
+  } catch (error) {
+    console.error('Error accessing display media.', error);
+  }
+}
+
+button.addEventListener('click', async () => {
+  if (!document.pictureInPictureElement) {
+    selectMediaStream();
+  }
+  else {
+    await document.exitPictureInPicture();
+    videoElement.srcObject.getTracks().forEach(track => track.stop());
+    videoElement.srcObject = null;
+    selectMediaStream();
+  }
+});
+
+// on load
+// selectMediaStream();
