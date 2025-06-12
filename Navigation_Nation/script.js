fb1214fcd40676122e6a4c085eb30e4f0f54dbf1
Dandee Galang <dandeegalang.ph@gmail.com>
2025-06-13 05:04:47 +0800

    polished js script

--- Navigation_Nation/script.js
+++ Navigation_Nation/script.js
@@ -5,6 +5,14 @@ const nav2 = document.getElementById("nav-2");
 const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
 const nav5 = document.getElementById("nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+function navAnimation(direction1, direction2) {
+    navItems.forEach((nav, index) => {
+        nav.classList.remove(`slide-${direction1}-${index + 1}`);
+        nav.classList.add(`slide-${direction2}-${index + 1}`);
+    });
+}
 
 function toggleNav() {
     // add class if it doesn't exist, remove if it does
@@ -12,31 +20,11 @@ function toggleNav() {
     menuBars.classList.toggle("change"); 
     overlay.classList.toggle("overlay-active");
     if (overlay.classList.contains("overlay-active")) {
-        overlay.classList.add("overlay-slide-right");
-        overlay.classList.remove("overlay-slide-left");
-        nav1.classList.remove("slide -out-1");
-        nav2.classList.remove("slide -out-2");
-        nav3.classList.remove("slide -out-3");
-        nav4.classList.remove("slide -out-4");
-        nav5.classList.remove("slide -out-5");
-        nav1.classList.add("slide -in-1");
-        nav2.classList.add("slide -in-2");
-        nav3.classList.add("slide -in-3");
-        nav4.classList.add("slide -in-4");
-        nav5.classList.add("slide -in-5");
+        overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+        navAnimation('out', 'in')
     } else {
-        overlay.classList.add("overlay-slide-left");
-        overlay.classList.remove("overlay-slide-right");
-        nav1.classList.remove("slide -in-1");
-        nav2.classList.remove("slide -in-2");
-        nav3.classList.remove("slide -in-3");
-        nav4.classList.remove("slide -in-4");
-        nav5.classList.remove("slide -in-5");
-        nav1.classList.add("slide -out-1");
-        nav2.classList.add("slide -out-2");
-        nav3.classList.add("slide -out-3");
-        nav4.classList.add("slide -out-4");
-        nav5.classList.add("slide -out-5");
+        overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+        navAnimation('in', 'out');
     }
 }
 
