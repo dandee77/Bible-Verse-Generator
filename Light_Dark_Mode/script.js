5a5b0145e09e56a7c74cafccd104fa0c61f58725
Dandee Galang <dandeegalang.ph@gmail.com>
2025-06-12 20:02:43 +0800

    added local storage

--- Light_Dark_Mode/script.js
+++ Light_Dark_Mode/script.js
@@ -31,11 +31,25 @@ function lightMode() {
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 toggleSwitch.addEventListener('change', switchTheme);
+
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;  
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    } else {
+        lightMode();
+    }
+}
